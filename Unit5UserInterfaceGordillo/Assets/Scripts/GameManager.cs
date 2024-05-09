7f624a8cd84a814c92f1b195c2ae0876ff05f5cb
Maximiliano Gordillo <724910@student.dusd.net>
2024-05-09 15:20:40 -0700

    finished 5.3
    added restart button almost finish. need to keep grinding. THE GRIND NEVER STOPS!!!

--- Unit5UserInterfaceGordillo/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceGordillo/Assets/Scripts/GameManager.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button RestartButton;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
-
-        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -29,7 +32,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -43,4 +46,15 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void GameOver()
+    {
+        RestartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
